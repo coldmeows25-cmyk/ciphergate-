cbc272588ffdf1b52cc00d36a21e96bb36e5aa62
coldmeows25-cmyk <coldmeows25@gmail.com>
2026-04-08 22:14:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("answer").addEventListener("keydown", function(e) {
   if (e.key === "Enter") {
     let input = this.value.toLowerCase();
 
-    if(input === "nyx") {
+    if(input === "ciphers") {
       document.getElementById("message").innerText = "> ACCESS GRANTED";
     } else {
       document.getElementById("message").innerText = "> ACCESS DENIED";
