60deee37bc65218b8d76a0a2def75d6f044e26ae
coldmeows25-cmyk <coldmeows25@gmail.com>
2026-04-09 07:28:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ document.getElementById("answer").addEventListener("keydown", function(e) {
     let input = this.value.toLowerCase();
 
     if(input === "ciphers") {
-      document.getElementById("message").innerText = "> ACCESS GRANTED";
+      document.body.innerHTML = `
+        <div style="color:lime; background:black; height:100vh; display:flex; justify-content:center; align-items:center; font-family:monospace; text-align:center;">
+          <div>
+            <h1>WELCOME TO CIPHERGATE</h1>
+            <p>> ACCESS GRANTED</p>
+            <p>> You are now inside the system</p>
+          </div>
+        </div>
+      `;
     } else {
       document.getElementById("message").innerText = "> ACCESS DENIED";
     }
