49ee217d8af62f3d911cb796bc0bb6299e73ef97
coldmeows25-cmyk <coldmeows25@gmail.com>
2026-04-09 07:48:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-document.getElementById("answer").addEventListener("keydown", function(e) {
-  if (e.key === "Enter") {
-    let input = this.value.toLowerCase();
+window.onload = function() {
+  const input = document.getElementById("answer");
 
-    if(input === "ciphers") {
-      document.body.innerHTML = `
-        <div style="color:lime; background:black; height:100vh; display:flex; justify-content:center; align-items:center; font-family:monospace; text-align:center;">
-          <div>
-            <h1>WELCOME TO CIPHERGATE</h1>
-            <p>> ACCESS GRANTED</p>
+  input.addEventListener("keydown", function(e) {
+    if (e.key === "Enter") {
+      let value = input.value.toLowerCase();
+
+      if (value === "ciphers") {
+        document.body.innerHTML = `
+          <div style="color:lime; background:black; height:100vh; display:flex; justify-content:center; align-items:center; font-family:monospace; text-align:center;">
+            <div>
+              <h1>WELCOME TO CIPHERGATE</h1>
+              <p>> ACCESS GRANTED</p>
+            </div>
           </div>
-        </div>
-      `;
-    } else {
-      document.getElementById("message").innerText = "> ACCESS DENIED";
+        `;
+      } else {
+        document.getElementById("message").innerText = "> ACCESS DENIED";
+      }
     }
-  }
-});
+  });
+};
